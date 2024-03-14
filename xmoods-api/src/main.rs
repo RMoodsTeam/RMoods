38e809808954e60c64ccd95e201f2cbe12a89194
Michał Miłek <michal.mileq@gmail.com>
2024-03-14 16:14:20 +0100

    Create a basic Rust project with one test
    Add a GH Action for running tests

---
+++ xmoods-api/src/main.rs
@@ -0,0 +1,13 @@
+/// Entry point of the XMoods server.
+/// Initializes the HTTP server and runs it.
+fn main() {
+    println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn example_test() {
+        assert!(2+2 == 4)
+    }
+}
