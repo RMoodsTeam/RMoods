8935374737fc744aaeef7f03cf0cc50d6d5a49da
Michał Miłek <michal.mileq@gmail.com>
2024-04-04 14:25:10 +0200

    Add a basic actix-web server scaffolding

---
+++ xmoods-api/src/auth/mod.rs
@@ -0,0 +1,18 @@
+//! Authentication module
+//!
+//! Verify the user's identity
+
+use actix_web::{get, web::{self, ServiceConfig}, HttpResponse, Responder};
+
+/// Index route for the `auth/` module
+/// Returns a welcome message
+#[get("")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello from /auth")
+}
+
+/// Router for the `auth/` module
+/// Adds all `auth/` services to the router
+pub fn router(cfg: &mut ServiceConfig) {
+    cfg.service(index);
+}
