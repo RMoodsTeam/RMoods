ef819e395442cd4c7cf9caa98eb565d7cd7fbea3
Katarzyna Jabłońska <147647574+katJablonska@users.noreply.github.com>
2024-06-06 19:41:57 +0200

    Add color palettes (#30)

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -7,6 +7,14 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      'primary-light': '#F8F5F1',
+      'secondary-light': '#DDE4E2',
+      'primary-dark': '#323447',
+      'secondary-dark': '#3C3D50',
+      'accent-green': '##28776C',
+      'accent-purple': '#AD2850',
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
