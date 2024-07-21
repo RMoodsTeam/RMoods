e59fe29b3c8e9ea12bf408ef2352ca08f922a38f
Sebastian Nowak <sebnow91@gmail.com>
2024-07-21 19:57:03 +0200

    test cypress

--- frontend/cypress/e2e/ThemeSwitch.cy.ts
+++ frontend/cypress/e2e/ThemeSwitch.cy.ts
@@ -1,5 +1,6 @@
 describe('Theme switch logic', () => {
   beforeEach(() => {
+    console.log("Before each!");
     cy.visit('localhost:3000/', {
       onBeforeLoad (win) {
         cy.stub(win, 'matchMedia')
@@ -10,8 +11,12 @@ describe('Theme switch logic', () => {
       },
     })
   })
+  it('Sees button', () => {
+    cy.get('#theme-switch')
+  })
 
   it('Detects users theme and switches it when button is clicked', () => {
+    console.log("before click!")
     cy.get('#theme-switch').click().should(() => {
       expect(localStorage.getItem("theme")).to.eq("dark");
     })
