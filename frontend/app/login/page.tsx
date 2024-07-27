d32d6c62979b5b3b4b30a7a6373884dedc74d218
Michał Miłek <michal.mileq@gmail.com>
2024-07-27 15:04:56 +0200

    Fix webpack/`use client` issue

---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,28 @@
+"use client"
+import React, { useState } from "react";
+import { useGoogleLogin } from "@react-oauth/google";
+import Cookies from "js-cookie";
+
+async function postGoogleCode(codeResponse: { code: string }) {
+    // for test purposes it will stay at this URL for now
+    const url = "http://localhost:8001/auth/auth/login";
+    const response = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ code: codeResponse.code }),
+    });
+    const answer = await response.json();
+    Cookies.set("auth", codeResponse.code, { expires: 30 });
+    console.log(answer);
+}
+
+export default function LoginPage() {
+    const googleLogin = useGoogleLogin({
+        onSuccess: (codeResponse) => postGoogleCode(codeResponse),
+        flow: "auth-code",
+    });
+
+    return (
+        <button onClick={() => googleLogin()}>ASD</button>
+    )
+}
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,17 +1,13 @@
-"use client";
-
-import { useState } from "react";
-import Login from "./components/Login";
+import Link from "next/link";
 /**
  * Home component, returns main content
  * @returns {TSX}
  */
 export default function Home() {
-  const [count, setCount] = useState(0);
   return (
     <div>
       <h1>RMoods</h1>
-      <Login />
+      <Link href={"/login"}>Go to Login</Link>
     </div>
   );
 }
