43f439237b8df9c9db25f84b14e34047874aa8ac
Michał Miłek <michal.mileq@gmail.com>
2024-07-28 20:01:56 +0200

    Improve custom Google button

--- frontend/app/login/GoogleSignInButton.tsx
+++ frontend/app/login/GoogleSignInButton.tsx
@@ -1,10 +1,16 @@
 export default function GoogleSignInButton(params: { onClick: any }) {
   return (
     <button
-      className="flex items-center justify-center w-full px-4 py-2 text-white bg-red-600 rounded-md"
       onClick={params.onClick}
+      className="px-4 py-2 flex gap-2 rounded-full border"
     >
-      Sign in with Google
+      <img
+        className="w-6 h-6"
+        src="https://www.svgrepo.com/show/475656/google-color.svg"
+        loading="lazy"
+        alt="google logo"
+      />
+      <span>Login with Google</span>
     </button>
   );
 }
