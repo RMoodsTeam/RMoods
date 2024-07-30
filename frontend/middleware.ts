30fac0a23cc8a2fcff54a24e61440b940fc4baf4
Michał Miłek <michal.mileq@gmail.com>
2024-07-30 20:34:13 +0200

    Fix middleware silently failing when JWT_SECRET missing

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,6 +1,6 @@
-import { NextResponse } from 'next/server'
-import { NextRequest } from 'next/server'
-import { jwtVerify } from 'jose';
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+import { jwtVerify } from "jose";
 
 export async function middleware(request: NextRequest) {
   // ***DO NOT CHANGE*** prevent rerouting NextJS assets like CSS
@@ -9,30 +9,36 @@ export async function middleware(request: NextRequest) {
   }
 
   // get pathname as we dont want to interrupt user if he wants to login
-  const {pathname} = request.nextUrl;
+  const { pathname } = request.nextUrl;
 
-  console.log(pathname)
+  console.log(pathname);
   if (pathname === "/login") {
-    return NextResponse.next()
+    return NextResponse.next();
   }
 
   // get token and then check if it's valid, if not, redirect to /login
   const token = request.cookies.get("RMOODS_JWT");
 
   if (!token) {
-    return NextResponse.redirect(new URL('/login', request.url))
+    return NextResponse.redirect(new URL("/login", request.url));
   }
 
-  // kill server if JWT_SECRET is not present
   // TODO: change missing env handling
   if (!process.env.JWT_SECRET) {
-    return NextResponse.redirect(new URL('/login', request.url))
+    console.error("JWT_SECRET not defined in the environment.");
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 },
+    );
   }
 
-  try{
-    await jwtVerify(token.value, new TextEncoder().encode(process.env.JWT_SECRET))
+  try {
+    await jwtVerify(
+      token.value,
+      new TextEncoder().encode(process.env.JWT_SECRET),
+    );
   } catch {
-    return NextResponse.redirect(new URL('/login', request.url))
+    return NextResponse.redirect(new URL("/login", request.url));
   }
-  return NextResponse.next()
+  return NextResponse.next();
 }
