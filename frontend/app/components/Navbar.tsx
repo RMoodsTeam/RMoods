656796bc8f419b455b7c6914b8e6dd65fd37346c
Sebastian Nowak <sebnow91@gmail.com>
2024-08-20 15:55:53 +0200

    Change Navbar to grid, update eslint config to use next config

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -9,14 +9,17 @@ import Link from "./Link";
  */
 const Navbar = () => {
   return (
-    <nav className="bg-primary-light dark:bg-primary-dark sticky top-0 z-50 w-full flex p-4 shadow-md place-content-around ">
+    <nav className="bg-primary-light dark:bg-primary-dark sticky top-0 z-50 w-full p-4 shadow-md grid grid-cols-[100px_100px_100px_1fr_100px] ">
       <Link href="/dashboard">Dashboard</Link>
       <Link href="/">Main</Link>
       {/* TODO!! move button to another file so the whole navbar is not a client component */}
       <Link href="/login" onClick={() => Cookies.remove("RMOODS_JWT")}>
         Log out
       </Link>
-      <ThemeSwitch />
+      {/* TODO!! fix the div and make ThemeSwitch take arguments*/}
+      <div className="col-start-5">
+        <ThemeSwitch />
+      </div>
     </nav>
   );
 };
