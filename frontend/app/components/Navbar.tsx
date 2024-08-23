13aa0595494881b054984387e7473d3fc8a39c51
Sebastian Nowak <sebnow91@gmail.com>
2024-08-23 22:50:21 +0200

    Fix navbar layout

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -9,10 +9,9 @@ import User from "./User";
  */
 const Navbar = () => {
   return (
-    <nav className="bg-primary-light dark:bg-primary-dark sticky top-0 z-50 w-full p-4 shadow-md grid grid-cols-[100px_100px_1fr_100px_100px] place-items-center">
+    <nav className="bg-primary-light dark:bg-primary-dark sticky top-0 z-50 w-full p-4 shadow-md grid grid-cols-[100px_1fr_100px_100px] place-items-center">
       <Link href="/">Main</Link>
-      <Logout />
-      <ThemeSwitch className="col-start-4" />
+      <ThemeSwitch className="col-start-3" />
       <User>User</User>
     </nav>
   );
