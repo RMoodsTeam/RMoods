4964aab6436ecc0abe767d56eb16138745488194
Sebastian Nowak <sebnow91@gmail.com>
2024-08-24 14:52:46 +0200

    fix e2e tests

--- frontend/app/login/GoogleSignInButton.tsx
+++ frontend/app/login/GoogleSignInButton.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Image from "next/image";
 
 /**
  * @param params - onClick function
@@ -12,7 +11,7 @@ export default function GoogleSignInButton(params: { onClick: any }) {
       className="px-4 py-2 flex gap-2 rounded-full border bg-primary-light items-center justify-center text-primary-dark hover:bg-primary-dark hover:text-primary-light transition-colors"
       id="google-sign-in-button"
     >
-      <Image
+      <img
         className="w-6 h-6"
         src="https://www.svgrepo.com/show/475656/google-color.svg"
         loading="lazy"
