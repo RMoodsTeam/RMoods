4f0450d610f7ad25dc3210c39d430dd8f779a3f0
Michał Miłek <michal.mileq@gmail.com>
2024-09-03 22:39:09 +0200

    MUI setup

--- frontend/app/Providers.tsx
+++ frontend/app/Providers.tsx
@@ -2,11 +2,18 @@
 import React from "react";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { Provider as JotaiProvider } from "jotai";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
+import theme from "./theme";
+import { ThemeProvider } from "@mui/material";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <GoogleOAuthProvider clientId="1055063718392-2ajj0s8h3pol9u5fdlt5vg8jep200r6i.apps.googleusercontent.com">
-      <JotaiProvider>{children}</JotaiProvider>
-    </GoogleOAuthProvider>
+    <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+      <ThemeProvider theme={theme}>
+        <GoogleOAuthProvider clientId="1055063718392-2ajj0s8h3pol9u5fdlt5vg8jep200r6i.apps.googleusercontent.com">
+          <JotaiProvider>{children}</JotaiProvider>
+        </GoogleOAuthProvider>
+      </ThemeProvider>
+    </AppRouterCacheProvider>
   );
 }
