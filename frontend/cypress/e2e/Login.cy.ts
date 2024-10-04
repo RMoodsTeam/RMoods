281f22a4ac3b1faf231e6b7af46a7d1f9124e619
Sebastian Nowak <sebnow91@gmail.com>
2024-10-04 22:12:20 +0200

    Add cypress test and docs building

---
+++ frontend/cypress/e2e/Login.cy.ts
@@ -0,0 +1,22 @@
+/// <reference types="cypress" />
+
+describe("Login", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:8000/login");
+  });
+
+  it("Displays a title", () => {
+    cy.get("#login-title").should("have.text", "Sign in to RMoods");
+  });
+
+  it("Displays Google sign in button", () => {
+    cy.get("#google-sign-in-button").should("be.visible");
+  });
+
+  it("Google button has the correct text", () => {
+    cy.get("#google-sign-in-button-text").should(
+      "have.text",
+      "Continue with Google",
+    );
+  });
+});
