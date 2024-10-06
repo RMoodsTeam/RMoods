22d78c266aa74532aed3039c68e8164300bd1647
Michał Miłek <105422839+m-milek@users.noreply.github.com>
2024-10-06 22:23:03 +0200

    119 improve theme switching (#120)
    * Add rought implementation of the theme switcher
    
    * Theme switching with Jotai's `atomWithStorage`
    
    * Add `dist` to .gitignore
    
    * Refactor
    
    * Add icons, refactor

--- frontend/src/atoms.ts
+++ frontend/src/atoms.ts
@@ -1,3 +1,10 @@
 import { atom } from "jotai";
+import { atomWithStorage } from "jotai/utils";
+import { RMoodsColorMode } from "./theme";
 
 export const userInfoAtom = atom<any>({});
+
+export const colorModeAtom = atomWithStorage<RMoodsColorMode>(
+  "COLOR_MODE",
+  (localStorage.getItem("COLOR_MODE") as RMoodsColorMode) || "light",
+);
--- frontend/src/components/navbar/ThemeSwitch.tsx
+++ frontend/src/components/navbar/ThemeSwitch.tsx
@@ -1,11 +1,64 @@
-import { Button, useColorMode } from "@chakra-ui/react";
+import { Button, Icon, useColorMode, HStack } from "@chakra-ui/react";
+import { FaSun, FaMoon, FaDesktop } from "react-icons/fa";
+import { getNextMode, getSystemMode } from "../../theme";
+import { useAtom } from "jotai";
+import { useEffect } from "react";
+import { colorModeAtom } from "../../atoms";
 
 const ThemeSwitch = () => {
   const { colorMode, toggleColorMode } = useColorMode();
+  const [trueColorMode, setTrueColorMode] = useAtom(colorModeAtom);
+
+  // Determine the current icon based on the mode
+  const getIcon = () => {
+    switch (trueColorMode) {
+      case "light":
+        return FaSun;
+      case "dark":
+        return FaMoon;
+      case "system":
+        return FaDesktop;
+      default:
+        return FaSun;
+    }
+  };
+
+  // Handle system color scheme preference changes
+  const handleSystemSchemeChange = () => {
+    const systemMode = getSystemMode();
+    if (trueColorMode === "system" && colorMode !== systemMode) {
+      toggleColorMode();
+    }
+  };
+
+  // Handle theme change logic
+  const onThemeChange = () => {
+    const nextMode = getNextMode(trueColorMode);
+    const systemMode = getSystemMode();
+
+    setTrueColorMode(nextMode);
+
+    if (
+      (nextMode === "system" && colorMode !== systemMode) ||
+      (nextMode !== "system" && nextMode !== colorMode)
+    ) {
+      toggleColorMode();
+    }
+  };
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    mediaQuery.addEventListener("change", handleSystemSchemeChange);
+    return () =>
+      mediaQuery.removeEventListener("change", handleSystemSchemeChange);
+  }, [colorMode, trueColorMode]);
+
   return (
     <header>
-      <Button onClick={toggleColorMode}>
-        Toggle to {colorMode === "light" ? "Dark" : "Light"}
+      <Button onClick={onThemeChange} aria-label="Toggle Theme" px={4} py={2}>
+        <HStack spacing={2}>
+          <Icon as={getIcon()} boxSize={5} />
+        </HStack>
       </Button>
     </header>
   );
