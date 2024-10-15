cebe712c1482af2c945684bee007f8c8579b9c3b
Sebastian Nowak <sebnow91@gmail.com>
2024-10-15 23:20:50 +0200

    Add sidebar placeholder content

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,22 +1,63 @@
 import {
   VStack,
   Heading,
-  Card,
+  Card, useDisclosure, Box, Collapse, Icon, Flex,
 } from "@chakra-ui/react";
+import {FaChevronDown, FaChevronRight } from "react-icons/fa";
+
+
+const SidebarSection = ({sectionTitle, children}) => {
+  const {isOpen, onToggle} = useDisclosure()
+
+  return (
+    <>
+      <Flex onClick={onToggle}
+      >
+        <Icon as={isOpen ? FaChevronDown  : FaChevronRight}/>
+        {sectionTitle}
+      </Flex >
+      <Collapse in={isOpen} animateOpacity>
+        <Box
+          marginLeft="24px"
+        >
+          {children}
+
+        </Box>
+      </Collapse>
+    </>
+  )
+}
 
 const Sidebar = () => {
 
   return (
-      <Card
-          margin={0}
-          minWidth="300px"
-          zIndex={1000}
-          marginY="2px"
-      >
-          <VStack align="stretch" p={4} spacing={4}>
-            <Heading size="md">Sidebar</Heading>
-          </VStack>
-      </Card>
+    <Card
+      margin={0}
+      width="300px"
+      zIndex={1}
+      marginY="2px"
+      overflow="hidden"
+    >
+        <Heading size="md">Sidebar</Heading>
+        <SidebarSection sectionTitle="Lorem">
+          <Box>Lorem ipsum dolor sit amet, consectetur adipisicing elit</Box>
+          <SidebarSection sectionTitle="Ipsum">
+            <Box>Assumenda, quia temporibus eveniet a libero incidunt suscipit</Box>
+            <SidebarSection sectionTitle="Ipsum">
+              <Box>Assumenda, quia temporibus eveniet a libero incidunt suscipit</Box>
+              <SidebarSection sectionTitle="Ipsum">
+                <Box>Assumenda, quia temporibus eveniet a libero incidunt suscipit</Box>
+                <SidebarSection sectionTitle="Ipsum">
+                  <Box>Assumenda, quia temporibus eveniet a libero incidunt suscipit</Box>
+                </SidebarSection>
+              </SidebarSection>
+            </SidebarSection>
+          </SidebarSection>
+          <Box>Quidem, ipsam illum quis sed voluptatum quae eum fugit earum</Box>
+          <Box>Quidem, ipsam illum quis sed voluptatum quae eum fugit earum</Box>
+
+        </SidebarSection>
+    </Card>
   );
 };
 
