30d222f4131f4a40d1c8866c87fa85d2370062cd
Sebastian Nowak <sebnow91@gmail.com>
2024-11-08 15:25:22 +0100

    131: Add mantine ui mockdata and icons

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,7 +1,55 @@
 import {Code, Group, ScrollArea} from '@mantine/core';
+import {
+  IconNotes,
+  IconCalendarStats,
+  IconGauge,
+  IconPresentationAnalytics,
+  IconFileAnalytics,
+  IconAdjustments,
+  IconLock,
+} from '@tabler/icons-react';
 import classes from './NavbarNested.module.css';
+import {LinksGroup} from "./NavbarLinksGroup.tsx";
 
-export default function Navbar() {
+const mockdata = [
+  { label: 'Dashboard', icon: IconGauge },
+  {
+    label: 'Market news',
+    icon: IconNotes,
+    initiallyOpened: true,
+    links: [
+      { label: 'Overview', link: '/' },
+      { label: 'Forecasts', link: '/' },
+      { label: 'Outlook', link: '/' },
+      { label: 'Real time', link: '/' },
+    ],
+  },
+  {
+    label: 'Releases',
+    icon: IconCalendarStats,
+    links: [
+      { label: 'Upcoming releases', link: '/' },
+      { label: 'Previous releases', link: '/' },
+      { label: 'Releases schedule', link: '/' },
+    ],
+  },
+  { label: 'Analytics', icon: IconPresentationAnalytics },
+  { label: 'Contracts', icon: IconFileAnalytics },
+  { label: 'Settings', icon: IconAdjustments },
+  {
+    label: 'Security',
+    icon: IconLock,
+    links: [
+      { label: 'Enable 2FA', link: '/' },
+      { label: 'Change password', link: '/' },
+      { label: 'Recovery codes', link: '/' },
+    ],
+  },
+];
+
+
+export default function Sidebar() {
+  const links = mockdata.map((item) => <LinksGroup {...item} key={item.label} />);
 
 // TODO: extract the styles to a variable
   return (
@@ -19,12 +67,12 @@ export default function Navbar() {
     }}>
       <div className={classes.header}>
         <Group justify="space-between">
-          <Code fw={700}>RMoods Logo</Code>
+          <Code fw={700}>v3.1.2</Code>
         </Group>
       </div>
 
       <ScrollArea className={classes.links}>
-        <div className={classes.linksInner}>Sidebar Placeholder</div>
+        <div className={classes.linksInner}>{links}</div>
       </ScrollArea>
 
       <div className={classes.footer}>
