f95d305faa3a17c0398965705e306a6f2f20c297
Katarzyna Jabłońska <kasiajab2002@gmail.com>
2024-11-11 20:01:19 +0100

    123: added basic footer design

---
+++ frontend/src/components/footer/Footer.tsx
@@ -0,0 +1,55 @@
+import { Anchor, Group, ActionIcon, rem } from '@mantine/core';
+import { IconBrandGithub, IconBrandTwitter, IconBrandYoutube} from '@tabler/icons-react';
+import TemporaryLogo from './TemporaryLogo';
+import classes from './FooterCentered.module.css';
+
+const links = [
+  { link: '#', label: 'Contact' },
+  { link: '#', label: 'Privacy' },
+  { link: '#', label: 'Blog' },
+  { link: '#', label: 'Store' },
+  { link: '#', label: 'Careers' },
+];
+
+export default function FooterCentered() {
+  const items = links.map((link) => (
+    <Anchor
+      c="dimmed"
+      key={link.label}
+      href={link.link}
+      lh={1}
+      onClick={(event) => event.preventDefault()}
+      size="sm"
+    >
+      {link.label}
+    </Anchor>
+  ));
+
+  return (
+    <footer className={classes.footer}>
+      <div className={classes.inner}>
+      <TemporaryLogo size={28} />
+
+        <Group className={classes.links}>{items}</Group>
+
+        <Group gap="xs" justify="flex-end" wrap="nowrap">
+        <Anchor href="https://github.com/RMoodsTeam/RMoods" target="_blank" rel="noopener noreferrer">
+          <ActionIcon size="lg" variant="default" radius="xl">
+            <IconBrandGithub style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
+          </ActionIcon>
+        </Anchor>
+        <Anchor href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" rel="noopener noreferrer">
+          <ActionIcon size="lg" variant="default" radius="xl">
+            <IconBrandTwitter style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
+          </ActionIcon>
+        </Anchor>
+          <Anchor href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" rel="noopener noreferrer">
+            <ActionIcon size="lg" variant="default" radius="xl">
+              <IconBrandYoutube style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
+            </ActionIcon>
+          </Anchor>
+        </Group>
+      </div>
+    </footer>
+  );
+}
---
+++ frontend/src/components/footer/TemporaryLogo.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TemporaryLogo = ({ size }: { size: number }) => (
+  <div style={{ fontSize: size, fontWeight: 'bold' }}>RMoods</div>
+);
+
+export default TemporaryLogo;
