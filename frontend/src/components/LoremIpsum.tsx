ac6ff363c50bd4e80c2adb67033b7aa6958b6ab2
Michał Miłek <michal.mileq@gmail.com>
2024-11-17 19:35:42 +0100

    142: Format

--- frontend/src/components/LoremIpsum.tsx
+++ frontend/src/components/LoremIpsum.tsx
@@ -1,21 +1,21 @@
 interface LoremIpsumProps {
-    n: number;
-  }
-  
-  const LoremIpsum = ({ n }: LoremIpsumProps) => {
-    const loremText = `Lorem ipsum odor amet, consectetuer adipiscing elit. Praesent in dignissim odio blandit lectus.
+  n: number;
+}
+
+const LoremIpsum = ({n}: LoremIpsumProps) => {
+  const loremText = `Lorem ipsum odor amet, consectetuer adipiscing elit. Praesent in dignissim odio blandit lectus.
       Erat aliquam sollicitudin suscipit, magnis vitae dapibus ex venenatis. Mus libero sodales amet eget mi augue.
       Facilisis magnis venenatis; quis viverra per habitasse quisque. Turpis venenatis blandit tempor condimentum
       neque volutpat sapien mollis. Dignissim eleifend vel aliquam at turpis cubilia quam sem ad. Himenaeos sit
       ullamcorper tempor sagittis fames.`;
-  
-    return (
-      <div>
-        {Array.from({ length: n }, (_, i) => (
-          <p key={i}>{loremText}</p>
-        ))}
-      </div>
-    );
-  };
-  
-  export default LoremIpsum;
+
+  return (
+    <div>
+      {Array.from({length: n}, (_, i) => (
+        <p key={i}>{loremText}</p>
+      ))}
+    </div>
+  );
+};
+
+export default LoremIpsum;
