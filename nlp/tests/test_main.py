86878eadb2b75aa52ecb73a1bc99b140225a30c9
Sebastian Jałoszyński <sebastian.jaloszynski@student.put.poznan.pl>
2024-11-23 20:15:52 +0100

    179: Delete old and unnecesarry tests

--- nlp/tests/test_main.py
+++ nlp/tests/test_main.py
@@ -6,25 +6,6 @@ from src.main import app
 @pytest.mark.asyncio
 @pytest.mark.parametrize("endpoint, expected_response", [
     ("/report/sentiment", {"sentiment": "I love this!"}),
-    ("/report/sarcasm", {"sarcasm": "I love this!"}),
-    ("/report/keywords", {"keywords": "I love this!"}),
-    ("/report/spam", {"spam": "I love this!"}),
-    ("/report/politics", {"politics": "I love this!"}),
-    ("/report/hate-speach", {"hate-speech": "I love this!"}),
-    ("/report/clickbait", {"clickbait": "I love this!"}),
-    ("/report/troll", {"troll": "I love this!"}),
-])
-async def test_endpoints(endpoint, expected_response):
-    async with AsyncClient(transport=ASGITransport(app=app),
-                           base_url="http://test") as ac:
-        response = await ac.post(endpoint, json={"text": ["I love this!"]})
-        assert response.status_code == 200
-        assert response.json() == expected_response
-
-
-@pytest.mark.asyncio
-@pytest.mark.parametrize("endpoint, expected_response", [
-    ("/report/sentiment", {"sentiment": "I love this!"}),
     ("/report/language", {"language": "I love this!"}),
     ("/report/sarcasm", {"sarcasm": "I love this!"}),
     ("/report/keywords", {"keywords": "I love this!"}),
