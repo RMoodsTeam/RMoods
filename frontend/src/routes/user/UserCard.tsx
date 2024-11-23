62c342df1578dba7b28af6dd70b9959a490d49ad
Michał Miłek <michal.mileq@gmail.com>
2024-11-23 23:50:18 +0100

    64: Use backend endpoint to fetch user data

--- frontend/src/routes/user/UserCard.tsx
+++ frontend/src/routes/user/UserCard.tsx
@@ -1,16 +1,7 @@
 import React from 'react';
-import { Box, Card, Divider, Text, Title } from '@mantine/core';
+import {Box, Divider, Text, Title} from '@mantine/core';
 import ProfilePicture from './ProfilePicture';
-
-/**
- * User interface representing the user data.
- */
-interface User {
-  name: string;
-  givenName: string;
-  email: string;
-  picture: string;
-}
+import {User} from './page.tsx';
 
 /**
  * Props for the UserCard component.
@@ -28,12 +19,24 @@ interface UserCardProps {
 const UserCard: React.FC<UserCardProps> = ({ user }) => {
   return (
     <Box style={{ width: '350px', height: '600px', padding: '20px' }}>
-      <Box style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <Box
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
         <ProfilePicture src={user.picture} alt={user.name} />
-        <Title order={2} style={{ marginTop: '10px', fontSize: '22px' }}>{user.name}</Title>
-        <Text size="sm" c="dimmed" style={{ fontSize: '18px' }}>{user.givenName}</Text>
+        <Title order={2} style={{ marginTop: '10px', fontSize: '22px' }}>
+          {user.name}
+        </Title>
+        <Text size="sm" c="dimmed" style={{ fontSize: '18px' }}>
+          {user.givenName}
+        </Text>
         <Divider my="sm" />
-        <Text size="sm" c="dimmed" style={{ fontSize: '18px' }}>{user.email}</Text>
+        <Text size="sm" c="dimmed" style={{ fontSize: '18px' }}>
+          {user.email}
+        </Text>
       </Box>
     </Box>
   );
--- frontend/src/routes/user/page.tsx
+++ frontend/src/routes/user/page.tsx
@@ -1,14 +1,16 @@
-import { Box, Text } from '@mantine/core';
+import {Box, Text} from '@mantine/core';
 import Cookies from 'js-cookie';
-import { jwtDecode } from "jwt-decode";
-import { useEffect, useState } from 'react';
+import {jwtDecode} from 'jwt-decode';
+import {useEffect, useState} from 'react';
 import UserCard from './UserCard';
 import StatisticItem from './StatisticItem';
+import {JwtClaims} from '../../rmoods/jwt.ts';
+import authFetch from '../../rmoods/client/authFetch.ts';
 
 /**
  * User interface representing the user data.
  */
-interface User {
+export interface User {
   name: string;
   givenName: string;
   email: string;
@@ -22,9 +24,7 @@ const statistics = {
   likedReports: ['Report 1', 'Report 2', 'Report 3'],
   latestReportDate: '2023-10-01',
   totalReports: 100,
-  topSubreddits: [
-    'subreddit1', 'subreddit2', 'subreddit3'
-  ],
+  topSubreddits: ['subreddit1', 'subreddit2', 'subreddit3'],
   karma: 5000,
   totalRequests: 200,
   generatedCost: 150.75,
@@ -39,24 +39,27 @@ const UserPage = () => {
   const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
-    const token = Cookies.get("RMOODS_JWT");
+    const token = Cookies.get('RMOODS_JWT');
     if (!token) {
-      console.error("No JWT token found");
-      throw new Error("No JWT token found");
+      console.error('No JWT token found');
+      throw new Error('No JWT token found');
     }
 
     try {
-      const decoded: any = jwtDecode(token);
-      const userInfo: User = {
-        name: decoded.user_info.name,
-        givenName: decoded.user_info.given_name,
-        email: decoded.user_info.email,
-        picture: decoded.user_info.picture,
-      };
-      setUser(userInfo);
+      const data = jwtDecode<JwtClaims>(token);
+      console.log('Decoded data: ', data);
+      const id = data.userInfo.id;
+
+      authFetch('http://localhost:8001/api/user?id=' + id, token).then(
+        (response) => {
+          response.json().then((data) => {
+            setUser(data);
+          });
+        }
+      );
     } catch (error) {
-      console.error("JWT token could not be decoded.");
-      throw new Error("JWT token could not be decoded.");
+      console.error('JWT token could not be decoded.', error);
+      throw new Error('JWT token could not be decoded.');
     }
   }, []);
 
@@ -65,22 +68,50 @@ const UserPage = () => {
   }
 
   return (
-    <Box style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start', margin: '50px 200px 0 200px' }}>
+    <Box
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'flex-start',
+        margin: '50px 200px 0 200px',
+      }}
+    >
       <Box style={{ flex: '0 0 350px', marginRight: '20px' }}>
         <UserCard user={user} />
       </Box>
       <Box style={{ flex: '1', display: 'flex', flexWrap: 'wrap' }}>
         <Box style={{ flex: '1 1 50%', padding: '10px' }}>
-          <StatisticItem label="Liked reports" value={statistics.likedReports.join(', ')} />
-          <StatisticItem label="Latest report" value={statistics.latestReportDate} />
-          <StatisticItem label="Number of created reports" value={statistics.totalReports} />
-          <StatisticItem label="Top 3 subreddits" value={statistics.topSubreddits.join(', ')} />
+          <StatisticItem
+            label="Liked reports"
+            value={statistics.likedReports.join(', ')}
+          />
+          <StatisticItem
+            label="Latest report"
+            value={statistics.latestReportDate}
+          />
+          <StatisticItem
+            label="Number of created reports"
+            value={statistics.totalReports}
+          />
+          <StatisticItem
+            label="Top 3 subreddits"
+            value={statistics.topSubreddits.join(', ')}
+          />
         </Box>
         <Box style={{ flex: '1 1 50%', padding: '10px' }}>
           <StatisticItem label="Karma" value={statistics.karma} />
-          <StatisticItem label="Total cost of reports" value={statistics.generatedCost} />
-          <StatisticItem label="Total used requests" value={statistics.totalRequests} />
-          <StatisticItem label="Longest Report Time" value={statistics.longestReportTime} />
+          <StatisticItem
+            label="Total cost of reports"
+            value={statistics.generatedCost}
+          />
+          <StatisticItem
+            label="Total used requests"
+            value={statistics.totalRequests}
+          />
+          <StatisticItem
+            label="Longest Report Time"
+            value={statistics.longestReportTime}
+          />
         </Box>
       </Box>
     </Box>
