ae13592e80f02422cf5db3853fc2988b2a0a74b6
Michał Miłek <michal.mileq@gmail.com>
2024-11-27 13:49:24 +0100

    175: Add Tanstack Query

--- frontend/src/providers/Providers.tsx
+++ frontend/src/providers/Providers.tsx
@@ -4,7 +4,8 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import WebsocketProvider from './WebsocketProvider.tsx';
 import { MantineProvider } from '@mantine/core';
 import { Notifications } from '@mantine/notifications';
-import { colorModeAtom } from './atoms.ts';
+import { colorModeAtom } from '../atoms.ts';
+import QueryProvider from './QueryProvider.tsx';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   const colorScheme = useAtomValue(colorModeAtom);
@@ -12,14 +13,16 @@ const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <StrictMode>
       <JotaiProvider>
-        <MantineProvider defaultColorScheme={colorScheme}>
-          <Notifications />
-          <WebsocketProvider>
-            <GoogleOAuthProvider clientId="1055063718392-2ajj0s8h3pol9u5fdlt5vg8jep200r6i.apps.googleusercontent.com">
-              {children}
-            </GoogleOAuthProvider>
-          </WebsocketProvider>
-        </MantineProvider>
+        <QueryProvider>
+          <MantineProvider defaultColorScheme={colorScheme}>
+            <Notifications />
+            <WebsocketProvider>
+              <GoogleOAuthProvider clientId="1055063718392-2ajj0s8h3pol9u5fdlt5vg8jep200r6i.apps.googleusercontent.com">
+                {children}
+              </GoogleOAuthProvider>
+            </WebsocketProvider>
+          </MantineProvider>
+        </QueryProvider>
       </JotaiProvider>
     </StrictMode>
   );
