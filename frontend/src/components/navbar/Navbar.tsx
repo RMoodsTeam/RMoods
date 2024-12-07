25687abda4925bb6533cf7bcaf759c24d4a72132
Sebastian Jałoszyński <sebastian.jaloszynski@student.put.poznan.pl>
2024-12-07 11:56:02 +0100

    212: Put nav items into Group component

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import UserMenu from './UserMenu';
 import ThemeSwitch from './ThemeSwitch';
-import { Anchor, Card, Flex, Grid } from '@mantine/core';
+import { Anchor, Card, Flex, Group } from '@mantine/core';
 import StatusIndicator from './StatusIndicator';
 import RateLimitStatus from './RateLimitStatus.tsx';
 
@@ -31,20 +31,13 @@ const RightNavItems = () => {
 const Navbar = () => {
   return (
     <Card
-      style={{ margin: 0, borderRadius: 0, marginBottom: 0, height: '10vh' }}
+      style={{ margin: 0, borderRadius: 0, marginBottom: 0 }}
     >
       <nav>
-        <Grid>
-          <Grid.Col span={4}>
-            <LeftNavItems />
-          </Grid.Col>
-          <Grid.Col span={4}>
-            <div />
-          </Grid.Col>
-          <Grid.Col span={4}>
-            <RightNavItems />
-          </Grid.Col>
-        </Grid>
+        <Group justify='space-between'>
+          <LeftNavItems />
+          <RightNavItems />
+        </Group>
       </nav>
     </Card>
   );
