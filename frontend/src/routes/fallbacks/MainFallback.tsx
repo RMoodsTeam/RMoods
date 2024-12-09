d2f47c977b21c52f99a6a243faae2e667ccd0992
Sebastian Nowak <sebnow91@gmail.com>
2024-12-09 13:38:47 +0100

    221: Add better main and page fallbacks

--- frontend/src/routes/fallbacks/MainFallback.tsx
+++ frontend/src/routes/fallbacks/MainFallback.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import { Card, Center, Stack, Text, Title } from '@mantine/core';
+import { IconAlertCircle } from '@tabler/icons-react';
 
-export const MainFallback = () => {
+export const MainFallback = ({ error }) => {
   return (
-    <div>
-      <h1>MainFallback</h1>
-    </div>
+    <Center>
+      <Card shadow="xs" p="xl" withBorder radius={20}>
+        <Stack>
+          <Center>
+            <IconAlertCircle size={48} color="red" />
+          </Center>
+          <Center>
+            <Title order={2}>
+              Critical failure! Something went seriously wrong
+            </Title>
+          </Center>
+          <Center>
+            <Text>
+              The app crashed. The devs will be notified and investigate the
+              issue.
+            </Text>
+          </Center>
+          <Center>
+            <Text size="sm" color="red">
+              {error.message}
+            </Text>
+          </Center>
+        </Stack>
+      </Card>
+    </Center>
   );
 };
--- frontend/src/routes/fallbacks/PageFallback.tsx
+++ frontend/src/routes/fallbacks/PageFallback.tsx
@@ -1,10 +1,29 @@
-import { Center } from '@mantine/core';
+import { Center, Stack, Title, Text, Paper, Card } from '@mantine/core';
+import { IconAlertCircle } from '@tabler/icons-react';
 
 export const PageFallback = ({ error }) => {
   return (
-    <Center role="alert">
-      <p>Something went wrong:</p>
-      <pre style={{ color: 'red' }}>{error.message}</pre>
+    <Center>
+      <Card shadow="xs" p="xl" withBorder radius={20}>
+        <Stack>
+          <Center>
+            <IconAlertCircle size={48} color="red" />
+          </Center>
+          <Center>
+            <Title order={2}>Oops! Something went wrong</Title>
+          </Center>
+          <Center>
+            <Text>
+              The page encountered an error and couldn't be displayed.
+            </Text>
+          </Center>
+          <Center>
+            <Text size="sm" color="red">
+              {error.message}
+            </Text>
+          </Center>
+        </Stack>
+      </Card>
     </Center>
   );
 };
