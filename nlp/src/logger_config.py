3076d0059c02d15577c1c587688dffb1e2bf13e2
Sebastian Jałoszyński <sebastian.jaloszynski@student.put.poznan.pl>
2024-12-09 19:57:44 +0100

    237: Add upload-language command in manager to upload specific model and language

--- nlp/src/logger_config.py
+++ nlp/src/logger_config.py
@@ -1,7 +1,7 @@
 import logging
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.DEBUG,
     format='%(levelname)s:     %(asctime)s - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
 )
