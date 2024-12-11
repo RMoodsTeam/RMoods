de1b75a71b44ec5737d84a186c958f4becb9372f
Michał Miłek <michal.mileq@gmail.com>
2024-12-12 00:16:11 +0100

    220: Dynamic SQL query

--- backend/src/db/impls/report_repository.rs
+++ backend/src/db/impls/report_repository.rs
@@ -6,22 +6,25 @@ use crate::db::pagination::DbPagination;
 use crate::nlp::analysis::NlpAnalysisKind;
 use crate::nlp::report::Report;
 use axum::async_trait;
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, NaiveDateTime, Utc};
 use sqlx::Error;
 
+#[derive(Debug, Clone)]
 pub struct DateRange {
     pub start_date: DateTime<Utc>,
     pub end_date: DateTime<Utc>,
 }
 
+#[derive(Debug, Clone)]
 pub struct ReportQuery {
-    pub user_id: Option<GoogleId>,
+    pub user_name_pattern: Option<GoogleId>,
     pub contained_analysis_kinds: Option<Vec<NlpAnalysisKind>>,
     pub date_range: Option<DateRange>,
     pub title_pattern: Option<String>,
 }
 
-pub(in crate::db) struct ReportQueryWhereClauses {
+#[derive(Debug)]
+pub(super) struct ReportQueryWhereClauses {
     user_id: String,
     contained_analysis_kinds: String,
     date_range: String,
@@ -29,11 +32,11 @@ pub(in crate::db) struct ReportQueryWhereClauses {
 }
 
 impl ReportQuery {
-    pub(in crate::db) fn into_where_clauses(self) -> ReportQueryWhereClauses {
+    pub(super) fn into_where_clauses(self) -> ReportQueryWhereClauses {
         let noop = "1=1".to_string();
 
         let user_id_clause = self
-            .user_id
+            .user_name_pattern
             .and_then(|id| Some(format!("user_id = {}", id)))
             .unwrap_or_else(|| noop.clone());
 
@@ -92,23 +95,45 @@ impl ReportRepository for Report {
     ) -> Result<Vec<Self>, Error> {
         let (limit, offset) = pagination.into_limit_and_offset();
 
-        let where_clauses = query.into_where_clauses();
-
-        let query: Vec<DbReport> = sqlx::query_as(
+        let query_str = format!(
             r#"
-        SELECT * FROM reports
-        WHERE ? AND ? AND ? AND ?
-        LIMIT ? OFFSET ?
+        SELECT * FROM reports r
+        JOIN users u ON r.user_id = u.id
+        JOIN report_metadata rm ON r.metadata_id = rm.id
+        JOIN report_analyses_maps maps ON r.analyses_map_id = maps.id
+        WHERE 
+        u.name LIKE '%$1%'
+        AND rm.report_created_at >= $2 AND rm.report_created_at <= $3
+        AND {}
+        AND r.title LIKE '%$4%'
+        LIMIT $5 OFFSET $6
         "#,
-        )
-        .bind(where_clauses.user_id)
-        .bind(where_clauses.contained_analysis_kinds)
-        .bind(where_clauses.date_range)
-        .bind(where_clauses.title_pattern)
-        .bind(limit)
-        .bind(offset)
-        .fetch_all(db.raw_db())
-        .await?;
+            query
+                .contained_analysis_kinds
+                .map(|kinds| {
+                    kinds
+                        .iter()
+                        .map(|kind| format!("maps.{}_id IS NOT NULL", kind.to_snake_case()))
+                        .collect::<Vec<_>>()
+                        .join(" AND ")
+                })
+                .unwrap_or("1=1".to_string())
+        );
+
+        let date_range = query.date_range.unwrap_or(DateRange {
+            start_date: NaiveDateTime::UNIX_EPOCH.and_utc(),
+            end_date: Utc::now(),
+        });
+
+        let query: Vec<DbReport> = sqlx::query_as(&query_str)
+            .bind(query.user_name_pattern.unwrap_or(GoogleId::from("")))
+            .bind(date_range.start_date)
+            .bind(date_range.end_date)
+            .bind(query.title_pattern.unwrap_or("".to_string()))
+            .bind(limit)
+            .bind(offset)
+            .fetch_all(db.raw_db())
+            .await?;
 
         let reports_fut = query
             .into_iter()
@@ -119,5 +144,135 @@ impl ReportRepository for Report {
             .collect::<Result<_, _>>()?;
 
         Ok(reports)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_into_where_clauses_user_id() {
+        let query = ReportQuery {
+            user_name_pattern: Some(GoogleId::from("test_user")),
+            contained_analysis_kinds: None,
+            date_range: None,
+            title_pattern: None,
+        };
+        let where_clauses = query.into_where_clauses();
+
+        assert_eq!(where_clauses.user_id, "user_id = test_user");
+    }
+
+    #[test]
+    fn test_into_where_clauses_contained_analysis_kinds_one() {
+        let query = ReportQuery {
+            user_name_pattern: None,
+            contained_analysis_kinds: Some(vec![NlpAnalysisKind::Sentiment]),
+            date_range: None,
+            title_pattern: None,
+        };
+        let where_clauses = query.into_where_clauses();
+
+        assert_eq!(
+            where_clauses.contained_analysis_kinds,
+            "analyses_map.sentiment_id IS NOT NULL"
+        );
+    }
+
+    #[test]
+    fn test_into_where_clauses_contained_analysis_kinds_multiple() {
+        let query = ReportQuery {
+            user_name_pattern: None,
+            contained_analysis_kinds: Some(vec![
+                NlpAnalysisKind::Sentiment,
+                NlpAnalysisKind::Clickbait,
+                NlpAnalysisKind::HateSpeech,
+            ]),
+            date_range: None,
+            title_pattern: None,
+        };
+        let where_clauses = query.into_where_clauses();
+
+        assert_eq!(
+            where_clauses.contained_analysis_kinds,
+            "analyses_map.sentiment_id IS NOT NULL AND analyses_map.clickbait_id IS NOT NULL AND analyses_map.hate_speech_id IS NOT NULL"
+        );
+    }
+
+    #[test]
+    fn test_into_where_clauses_date_range() {
+        let now = Utc::now();
+        let query = ReportQuery {
+            user_name_pattern: None,
+            contained_analysis_kinds: None,
+            date_range: Some(DateRange {
+                start_date: now,
+                end_date: now,
+            }),
+            title_pattern: None,
+        };
+        let where_clauses = query.into_where_clauses();
+
+        assert_eq!(
+            where_clauses.date_range,
+            format!("created_at >= {} AND created_at <= {}", now, now)
+        );
+    }
+
+    #[test]
+    fn test_into_where_clauses_title_pattern() {
+        let query = ReportQuery {
+            user_name_pattern: None,
+            contained_analysis_kinds: None,
+            date_range: None,
+            title_pattern: Some("test".to_string()),
+        };
+        let where_clauses = query.into_where_clauses();
+
+        assert_eq!(where_clauses.title_pattern, "title LIKE '%test%'");
+    }
+
+    #[sqlx::test]
+    async fn test_get_by_query_all_none() {
+        dotenvy::dotenv().ok();
+        let db_url = std::env::var("DATABASE_URL").expect("DATABASE_URL is set");
+        let db = DbClient::new(sqlx::PgPool::connect(&db_url).await.unwrap());
+
+        let reports_res = Report::get_by_query(
+            ReportQuery {
+                user_name_pattern: None,
+                contained_analysis_kinds: None,
+                date_range: None,
+                title_pattern: None,
+            },
+            DbPagination::new(0, 10),
+            &db,
+        )
+        .await
+        .unwrap();
+    }
+
+    #[sqlx::test]
+    async fn test_get_by_query_all_filled() {
+        dotenvy::dotenv().ok();
+        let db_url = std::env::var("DATABASE_URL").unwrap();
+        let db = DbClient::new(sqlx::PgPool::connect(&db_url).await.unwrap());
+
+        let reports_res = Report::get_by_query(
+            ReportQuery {
+                user_name_pattern: Some(GoogleId::from("test_user")),
+                contained_analysis_kinds: Some(vec![NlpAnalysisKind::Sentiment]),
+                date_range: Some(DateRange {
+                    start_date: Utc::now() - chrono::Duration::days(1),
+                    end_date: Utc::now(),
+                }),
+                title_pattern: Some("test".to_string()),
+            },
+            DbPagination::new(0, 10),
+            &db,
+        )
+        .await
+        .unwrap();
     }
 }
--- backend/src/db/pagination.rs
+++ backend/src/db/pagination.rs
@@ -1,15 +1,25 @@
 /// Pagination struct for database queries that would return too much data otherwise.
 pub struct DbPagination {
     page: u32,
-    per_page: u32,
+    per_page: u8,
 }
 
 impl DbPagination {
+    pub fn new(page: u32, per_page: u8) -> Self {
+        Self { page, per_page }
+    }
+
     /// Indexed from 1, so the first page is denoted by "1".
     ///
     /// Returned as a tuple of [i64] due to SQLx's query! macro requiring i64 for LIMIT and OFFSET.
     pub fn into_limit_and_offset(self) -> (i64, i64) {
-        let offset = (self.page - 1) * self.per_page;
+        let page_checked = if self.page == 0 {
+            log::warn!("Page was passed as 0, setting it to 1");
+            1
+        } else {
+            self.page
+        };
+        let offset = (page_checked - 1) * self.per_page as u32;
         (self.per_page as i64, offset as i64)
     }
 }
