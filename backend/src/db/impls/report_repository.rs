d143999355b144cbac9947828a88b99e542eb1b0
Michał Miłek <michal.mileq@gmail.com>
2024-12-12 01:12:32 +0100

    220: Improve SQL generation

--- backend/src/db/impls/report_repository.rs
+++ backend/src/db/impls/report_repository.rs
@@ -1,4 +1,3 @@
-use crate::auth::google::GoogleId;
 use crate::db::db_client::DbClient;
 use crate::db::from_db::FromDb;
 use crate::db::model::DbReport;
@@ -17,62 +16,61 @@ pub struct DateRange {
 
 #[derive(Debug, Clone)]
 pub struct ReportQuery {
-    pub user_name_pattern: Option<GoogleId>,
+    pub user_name_pattern: Option<String>,
     pub contained_analysis_kinds: Option<Vec<NlpAnalysisKind>>,
     pub date_range: Option<DateRange>,
     pub title_pattern: Option<String>,
 }
 
-#[derive(Debug)]
-pub(super) struct ReportQueryWhereClauses {
-    user_id: String,
-    contained_analysis_kinds: String,
-    date_range: String,
+impl Default for ReportQuery {
+    fn default() -> Self {
+        Self {
+            user_name_pattern: None,
+            contained_analysis_kinds: None,
+            date_range: None,
+            title_pattern: None,
+        }
+    }
+}
+
+struct ReportQueryBindArgs {
+    user_name_pattern: String,
+    contained_analysis_kinds_clauses: String,
+    start_date: DateTime<Utc>,
+    end_date: DateTime<Utc>,
     title_pattern: String,
 }
 
 impl ReportQuery {
-    pub(super) fn into_where_clauses(self) -> ReportQueryWhereClauses {
-        let noop = "1=1".to_string();
-
-        let user_id_clause = self
-            .user_name_pattern
-            .and_then(|id| Some(format!("user_id = {}", id)))
-            .unwrap_or_else(|| noop.clone());
-
-        let contained_analysis_kinds_clause = match self.contained_analysis_kinds {
-            Some(kinds) if kinds.is_empty() => noop.clone(),
-            Some(kinds) => {
-                let kind_clauses = kinds
+    fn into_bind_args(self) -> ReportQueryBindArgs {
+        let user_name_pattern = self.user_name_pattern.unwrap_or(String::new());
+        let contained_analysis_kinds_clauses = self
+            .contained_analysis_kinds
+            .map(|kinds| {
+                kinds
                     .iter()
-                    .map(|kind| format!("analyses_map.{}_id IS NOT NULL", kind.to_snake_case()))
+                    .map(|kind| {
+                        format!(
+                            "report_analyses_maps.{}_id IS NOT NULL",
+                            kind.to_snake_case()
+                        )
+                    })
                     .collect::<Vec<_>>()
-                    .join(" AND ");
-                kind_clauses
-            }
-            None => noop.clone(),
-        };
-
-        let date_range_clause = self
-            .date_range
-            .and_then(|range| {
-                Some(format!(
-                    "created_at >= {} AND created_at <= {}",
-                    range.start_date, range.end_date
-                ))
+                    .join(" AND ")
             })
-            .unwrap_or_else(|| noop.clone());
+            .unwrap_or("1=1".to_string());
+        let date_range = self.date_range.unwrap_or(DateRange {
+            start_date: NaiveDateTime::UNIX_EPOCH.and_utc(),
+            end_date: Utc::now(),
+        });
+        let title_pattern = self.title_pattern.unwrap_or("".to_string());
 
-        let title_pattern_clause = self
-            .title_pattern
-            .and_then(|pattern| Some(format!("title LIKE '%{}%'", pattern)))
-            .unwrap_or_else(|| noop);
-
-        ReportQueryWhereClauses {
-            user_id: user_id_clause,
-            contained_analysis_kinds: contained_analysis_kinds_clause,
-            date_range: date_range_clause,
-            title_pattern: title_pattern_clause,
+        ReportQueryBindArgs {
+            user_name_pattern,
+            contained_analysis_kinds_clauses,
+            start_date: date_range.start_date,
+            end_date: date_range.end_date,
+            title_pattern,
         }
     }
 }
@@ -95,12 +93,14 @@ impl ReportRepository for Report {
     ) -> Result<Vec<Self>, Error> {
         let (limit, offset) = pagination.into_limit_and_offset();
 
+        let bind_args = query.into_bind_args();
+
         let query_str = format!(
             r#"
         SELECT * FROM reports r
         JOIN users u ON r.user_id = u.id
         JOIN report_metadata rm ON r.metadata_id = rm.id
-        JOIN report_analyses_maps maps ON r.analyses_map_id = maps.id
+        JOIN report_analyses_maps ON r.analyses_map_id = report_analyses_maps.id
         WHERE 
         u.name LIKE '%$1%'
         AND rm.report_created_at >= $2 AND rm.report_created_at <= $3
@@ -108,28 +108,14 @@ impl ReportRepository for Report {
         AND r.title LIKE '%$4%'
         LIMIT $5 OFFSET $6
         "#,
-            query
-                .contained_analysis_kinds
-                .map(|kinds| {
-                    kinds
-                        .iter()
-                        .map(|kind| format!("maps.{}_id IS NOT NULL", kind.to_snake_case()))
-                        .collect::<Vec<_>>()
-                        .join(" AND ")
-                })
-                .unwrap_or("1=1".to_string())
+            bind_args.contained_analysis_kinds_clauses
         );
 
-        let date_range = query.date_range.unwrap_or(DateRange {
-            start_date: NaiveDateTime::UNIX_EPOCH.and_utc(),
-            end_date: Utc::now(),
-        });
-
         let query: Vec<DbReport> = sqlx::query_as(&query_str)
-            .bind(query.user_name_pattern.unwrap_or(GoogleId::from("")))
-            .bind(date_range.start_date)
-            .bind(date_range.end_date)
-            .bind(query.title_pattern.unwrap_or("".to_string()))
+            .bind(bind_args.user_name_pattern)
+            .bind(bind_args.start_date)
+            .bind(bind_args.end_date)
+            .bind(bind_args.title_pattern)
             .bind(limit)
             .bind(offset)
             .fetch_all(db.raw_db())
@@ -150,53 +136,56 @@ impl ReportRepository for Report {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::default::Default;
 
     #[test]
     fn test_into_where_clauses_user_id() {
         let query = ReportQuery {
-            user_name_pattern: Some(GoogleId::from("test_user")),
-            contained_analysis_kinds: None,
-            date_range: None,
-            title_pattern: None,
+            user_name_pattern: Some(String::from("test_user")),
+            ..Default::default()
         };
-        let where_clauses = query.into_where_clauses();
+        let args = query.into_bind_args();
 
-        assert_eq!(where_clauses.user_id, "user_id = test_user");
+        assert_eq!(args.user_name_pattern, "test_user");
+    }
+
+    #[test]
+    fn test_into_where_clauses_user_id_empty() {
+        let query = ReportQuery::default();
+        let args = query.into_bind_args();
+
+        assert_eq!(args.user_name_pattern, "");
     }
 
     #[test]
     fn test_into_where_clauses_contained_analysis_kinds_one() {
         let query = ReportQuery {
-            user_name_pattern: None,
             contained_analysis_kinds: Some(vec![NlpAnalysisKind::Sentiment]),
-            date_range: None,
-            title_pattern: None,
+            ..Default::default()
         };
-        let where_clauses = query.into_where_clauses();
+        let args = query.into_bind_args();
 
         assert_eq!(
-            where_clauses.contained_analysis_kinds,
-            "analyses_map.sentiment_id IS NOT NULL"
+            args.contained_analysis_kinds_clauses,
+            "report_analyses_maps.sentiment_id IS NOT NULL"
         );
     }
 
     #[test]
     fn test_into_where_clauses_contained_analysis_kinds_multiple() {
         let query = ReportQuery {
-            user_name_pattern: None,
             contained_analysis_kinds: Some(vec![
                 NlpAnalysisKind::Sentiment,
                 NlpAnalysisKind::Clickbait,
                 NlpAnalysisKind::HateSpeech,
             ]),
-            date_range: None,
-            title_pattern: None,
+            ..Default::default()
         };
-        let where_clauses = query.into_where_clauses();
+        let args = query.into_bind_args();
 
         assert_eq!(
-            where_clauses.contained_analysis_kinds,
-            "analyses_map.sentiment_id IS NOT NULL AND analyses_map.clickbait_id IS NOT NULL AND analyses_map.hate_speech_id IS NOT NULL"
+            args.contained_analysis_kinds_clauses,
+            "report_analyses_maps.sentiment_id IS NOT NULL AND report_analyses_maps.clickbait_id IS NOT NULL AND report_analyses_maps.hate_speech_id IS NOT NULL"
         );
     }
 
@@ -204,33 +193,47 @@ mod tests {
     fn test_into_where_clauses_date_range() {
         let now = Utc::now();
         let query = ReportQuery {
-            user_name_pattern: None,
-            contained_analysis_kinds: None,
             date_range: Some(DateRange {
-                start_date: now,
+                start_date: now - chrono::Duration::days(1),
                 end_date: now,
             }),
-            title_pattern: None,
+            ..Default::default()
         };
-        let where_clauses = query.into_where_clauses();
+        let args = query.into_bind_args();
 
-        assert_eq!(
-            where_clauses.date_range,
-            format!("created_at >= {} AND created_at <= {}", now, now)
-        );
+        assert_eq!(args.start_date, now - chrono::Duration::days(1));
+        assert_eq!(args.end_date, now);
+    }
+
+    #[test]
+    fn test_into_where_clauses_date_range_empty() {
+        let now = Utc::now();
+        let query = ReportQuery::default();
+        let args = query.into_bind_args();
+
+        assert_eq!(args.start_date, NaiveDateTime::UNIX_EPOCH.and_utc());
+        // We need to check the time as well, as the Utc::now() call in the test and the one in the function might be a few milliseconds apart
+        // This is why we check if the difference is less than a second
+        assert!(args.end_date - now < chrono::Duration::seconds(1));
     }
 
     #[test]
     fn test_into_where_clauses_title_pattern() {
         let query = ReportQuery {
-            user_name_pattern: None,
-            contained_analysis_kinds: None,
-            date_range: None,
             title_pattern: Some("test".to_string()),
+            ..Default::default()
         };
-        let where_clauses = query.into_where_clauses();
+        let args = query.into_bind_args();
 
-        assert_eq!(where_clauses.title_pattern, "title LIKE '%test%'");
+        assert_eq!(args.title_pattern, "test");
+    }
+
+    #[test]
+    fn test_into_where_clauses_title_pattern_empty() {
+        let query = ReportQuery::default();
+        let args = query.into_bind_args();
+
+        assert_eq!(args.title_pattern, "");
     }
 
     #[sqlx::test]
@@ -239,7 +242,7 @@ mod tests {
         let db_url = std::env::var("DATABASE_URL").expect("DATABASE_URL is set");
         let db = DbClient::new(sqlx::PgPool::connect(&db_url).await.unwrap());
 
-        let reports_res = Report::get_by_query(
+        let _ = Report::get_by_query(
             ReportQuery {
                 user_name_pattern: None,
                 contained_analysis_kinds: None,
@@ -259,12 +262,15 @@ mod tests {
         let db_url = std::env::var("DATABASE_URL").unwrap();
         let db = DbClient::new(sqlx::PgPool::connect(&db_url).await.unwrap());
 
-        let reports_res = Report::get_by_query(
+        let _ = Report::get_by_query(
             ReportQuery {
-                user_name_pattern: Some(GoogleId::from("test_user")),
-                contained_analysis_kinds: Some(vec![NlpAnalysisKind::Sentiment]),
+                user_name_pattern: Some(String::from("test_user")),
+                contained_analysis_kinds: Some(vec![
+                    NlpAnalysisKind::Sentiment,
+                    NlpAnalysisKind::HateSpeech,
+                ]),
                 date_range: Some(DateRange {
-                    start_date: Utc::now() - chrono::Duration::days(1),
+                    start_date: Utc::now() - chrono::Duration::days(3),
                     end_date: Utc::now(),
                 }),
                 title_pattern: Some("test".to_string()),
