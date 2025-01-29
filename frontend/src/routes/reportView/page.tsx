2c3d6f74782d2adfc05c8c9458791cf5cc3dbd6f
Sebastian Nowak <sebnow91@gmail.com>
2025-01-29 20:34:29 +0100

    259: Add a template of report view

--- frontend/src/routes/reportView/page.tsx
+++ frontend/src/routes/reportView/page.tsx
@@ -1,15 +1,34 @@
-import { Accordion, Card, Flex, Stack } from '@mantine/core';
+import {
+  Accordion,
+  Card,
+  Combobox,
+  Flex,
+  Stack,
+  Text,
+  Title,
+} from '@mantine/core';
 import { ErrorBoundary } from 'react-error-boundary';
 import { PageFallback } from '../PageFallback.tsx';
 import styles from './page.module.scss';
+import { RMoodsClient } from '../../rmoods/client/RMoodsClient.ts';
+import { useQuery } from '@tanstack/react-query';
+import Header = Combobox.Header;
+import dayjs from 'dayjs';
+
+const fetchReports = async (params: URLSearchParams) => {
+  const queryResponse = await RMoodsClient.fetchUserReports(params);
+  return queryResponse;
+};
 
 const ReportElement = ({ value, description }) => {
   return (
     <Card className={styles.item}>
-      <Accordion>
-        <Accordion.Item key={value} value={value}>
+      <Accordion radius="lg">
+        <Accordion.Item value={value} className={styles.accordion}>
           <Accordion.Control>{value}</Accordion.Control>
-          <Accordion.Panel>{description}</Accordion.Panel>
+          <Accordion.Panel className={styles.accordionInner}>
+            {description}
+          </Accordion.Panel>
         </Accordion.Item>
       </Accordion>
     </Card>
@@ -17,11 +36,56 @@ const ReportElement = ({ value, description }) => {
 };
 
 const ReportView = () => {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['reportsView', '?per_page=30&mine=true&page=1'],
+    queryFn: () =>
+      fetchReports(new URLSearchParams('?per_page=30&mine=true&page=1')),
+  });
+
+  const reportMetadata = data?.reports[1];
+
+  const firstReport = data?.reports[1].analyses
+    ? Object.entries(data.reports[1].analyses)
+    : [];
+
   return (
+    // <Stack className={styles.wrapper}>
+    //   <Title order={1}>{reportMetadata?.title} </Title>
+    //   <Text> {reportMetadata?.description}</Text>
+    //   <Text>
+    //     {' '}
+    //     Created at{' '}
+    //     {dayjs(reportMetadata?.created_at).format('YYYY-MM-DD HH:mm')}
+    //   </Text>
+    //   {firstReport.map((item) => {
+    //     console.log(item[0]);
+    //     return (
+    //       <ReportElement
+    //         key={item[0]}
+    //         value={item[0]}
+    //         description={JSON.stringify(item[1], null, 2)}
+    //       />
+    //     );
+    //   })}
+    // </Stack>
     <Stack className={styles.wrapper}>
-      <ReportElement value="item.value1" description="item.description" />
-      <ReportElement value="item.value2" description="item.description" />
-      <ReportElement value="item.value3" description="item.description" />
+      <Title order={1}> {reportMetadata?.title} </Title>
+      <Text> Description </Text>
+      <Text>
+        {' '}
+        Created at{' '}
+        {dayjs(reportMetadata?.created_at).format('YYYY-MM-DD HH:mm')}
+      </Text>
+      {firstReport.map((item) => {
+        console.log(item[0]);
+        return (
+          <ReportElement
+            key={item[0]}
+            value={item[0]}
+            description={JSON.stringify(item[1], null, 2)}
+          />
+        );
+      })}
     </Stack>
   );
 };
